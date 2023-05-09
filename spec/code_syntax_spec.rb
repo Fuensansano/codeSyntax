2746ea47c27811b33decf1d06be51fec209f689a
Fuen Sansano <fuensansano@gmail.com>
2023-05-09 21:40:24 +0200

    GREEN: given <><><> then return true

--- spec/code_syntax_spec.rb
+++ spec/code_syntax_spec.rb
@@ -13,6 +13,11 @@ describe Linter do
     expect(linter.parse('<><>')).to be(true)
   end
 
+  it 'given <><><> then return true' do
+    linter = Linter.new
+
+    expect(linter.parse('<><><>')).to be(true)
+  end
 
   ['><', '>','<'].each do |code_syntax|
     it "given #{code_syntax} then return false" do
--- src/linter.rb
+++ src/linter.rb
@@ -2,6 +2,7 @@ class Linter
   def parse(code_syntax)
     return true if code_syntax === '<>'
     return true if code_syntax === '<><>'
+    return true if code_syntax === '<><><>'
     false
   end
 end
